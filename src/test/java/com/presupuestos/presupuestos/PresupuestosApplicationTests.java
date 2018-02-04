1aae8d9245bfbf687dfc68d8966de2f393d303ff
romadiz <rodizma@gmail.com>
2018-02-04 12:33:17 +0100

    Subida inicial

---
+++ src/test/java/com/presupuestos/presupuestos/PresupuestosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.presupuestos.presupuestos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PresupuestosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
